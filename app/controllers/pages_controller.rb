51ccdad06ac1738e797596d62db833d29e317986
Ryan Cusick <rcusick82@gmail.com>
2018-10-02 08:35:23 -0400

    added html for search/results pages/ fixed image pathways

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,7 @@ class PagesController < ApplicationController
 
   def results
   end
+
+  def home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'pages/partners'
   get 'pages/search'
   get 'pages/results'
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
