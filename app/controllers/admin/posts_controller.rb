d7fe8eb9b6978d12e472d68d4705a9955b903926
Matt Casedonte <mcasedonte@gmail.com>
2018-10-09 15:45:28 -0400

    Added new/edit functionality and views for admin/posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,26 @@
 class Admin::PostsController < ApplicationController
   def index
+
   end
 
-  def show
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(title:params[:post][:title], body:params[:post][:body])
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id]
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    @post.save
+    redirect_to posts_path
   end
 end
