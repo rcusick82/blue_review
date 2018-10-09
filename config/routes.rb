6866ccec0c2bb872e7d4aa44f0d9465182f746fa
Matt Casedonte <mcasedonte@gmail.com>
2018-10-09 11:31:58 -0400

    Added basic implementation of posts/index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'pages/partners'
   get 'pages/search'
   get 'pages/results'
-  get 'pages/home'
+  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
